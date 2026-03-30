b0d7766726909ac6ef969db680734a0cf7355789
Billy Weeks <meznull43628@gmail.com>
2026-03-29 20:11:22 -0700

    Created and conencted "style" to .html and added "mount" logic to main.py

--- app/main.py
+++ app/main.py
@@ -1,11 +1,15 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates ## For reading HTML templates
+from fastapi.staticfiles import StaticFiles ## For taking care of static files like CSS
 
 ## Create FastAPI app
 app = FastAPI()
 
 ## Point Jinja to correct directory holding templates
 templates = Jinja2Templates(directory="templates")
+
+## Mount the static files directory to serve CSS and other static assets
+app.mount("/static", StaticFiles(directory="static"), name="static"
 
 
 ##  Decorator for the root endpoint and then define 
