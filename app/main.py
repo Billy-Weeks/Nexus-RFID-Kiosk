0a650abc596c6bfb581b5b5dc80f414e9e2daeec
Billy Weeks <meznull43628@gmail.com>
2026-03-29 19:38:35 -0700

    Created html.index and updated main to connect to that template

--- app/main.py
+++ app/main.py
@@ -1,6 +1,4 @@
-from re import M
-from urllib import request
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates ## For reading HTML templates
 
 ## Create FastAPI app
@@ -13,12 +11,12 @@ templates = Jinja2Templates(directory="templates")
 ##  Decorator for the root endpoint and then define 
 ##  the function that will be called when the root endpoint is accessed
 @app.get("/")
-def read_root():
+def read_root(request: Request):
     """
         Root endpoint function
         name => Name of the HTML template to render
         context => Context to pass to the template, including a welcome message
     """
-    name = "index.html"
-    context ={"request": request, "message": "Welcome to the Kiosk"}
-    return templates.TemplateResponse(name, context) ## Render the template with the provided context
+    return templates.TemplateResponse(request=request,
+                                      name="index.html",
+                                      context={"message": "Welcome to the Kiosk"}) 
