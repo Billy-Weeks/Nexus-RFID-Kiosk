eda048b261db9d2bdbe1484b70c4df1dc4c20e9d
Billy Weeks <meznull43628@gmail.com>
2026-05-16 17:32:19 -0700

    Made changes to launcher.py to create a more deployable app

---
+++ app/launcher.py
@@ -0,0 +1,21 @@
+#   Imports
+import uvicorn ##  Gives the ability to run the server
+import webbrowser ##  Gives the ability to open a web browser (Should be OS independent)
+import threading ##  Gives the ability to run multiple processes at the same time (Used to run server and open browser at the same time)
+import time ##  Give ability to "sleep"
+from app.main import app ##  Imports the FastAPI app from main.py
+
+##  Refactor to use operating system independent code to open browser in "kiosk" mode
+
+def start_browser():
+    time.sleep(2)
+    webbrowser.open("http://127.0.0.1:8000")
+
+
+
+if __name__ == "__main__":
+    ##  Start browser timer in a separate thread (background)
+    threading.Thread(target=start_browser, daemon=True).start()
+
+    ##  Start the server (Inside Python, no command prompt)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
